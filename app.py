634b3886148db7defed5f7b5fda82cea6b553a27
StevenDelval <asteven59220@free.fr>
2022-08-09 10:25:30 +0200

    Import model machine learning

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.compose import  ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder,StandardScaler,PolynomialFeatures
-from sklearn.linear_model import Ridge
-from sklearn.pipeline import make_pipeline
+import pickle 
 
 
 X_train = pd.read_csv('csv/X_train.csv')
@@ -12,30 +9,12 @@ X_test = pd.read_csv('csv/X_test.csv')
 y_train = pd.read_csv('csv/y_train.csv')
 y_test = pd.read_csv('csv/y_test.csv') 
 
-numerical_features = [
-       'bedrooms', 'bathrooms',
-       'sqft_living', 'sqft_living_carre', 'sqft_lot','sqft_above',
-       'sqft_basement','sqft_living15', 'sqft_lot15'
-       ]
-categorical_features = [
-       'floors','waterfront','view', 'condition', 'grade','zipcode'
-       ]
-
-my_col_trans = ColumnTransformer([
-    ("sclal",StandardScaler(),numerical_features),
-    ("pol",PolynomialFeatures(),numerical_features),
-    ("one_hot_encoder",OneHotEncoder(handle_unknown = 'ignore'),categorical_features)
-])
-
-
-modelRid= make_pipeline(my_col_trans, Ridge(alpha=7.5,random_state=0))
-modelRid.fit(X_train,y_train)
+pickle_in = open('modelRid.pkl', 'rb') 
+modelRid = pickle.load(pickle_in) 
 
 titre = "Prédiction prix d'une maison"
 original_title = '<p style="font-family:Courier; color:Blue; font-size: 42px;">{} </p>'.format(titre)
 st.markdown(original_title, unsafe_allow_html=True)
-
-
 
 caracteristique_maison = [0 for _ in range(17)]
 
@@ -105,7 +84,6 @@ caracteristique_maison[16] = sqft_lot15
 
 
 if(st.button("Valider")):
-    
     
     predic = int(modelRid.predict(pd.DataFrame(np.array(caracteristique_maison).reshape(1, -1),columns=X_train.columns)))
     
